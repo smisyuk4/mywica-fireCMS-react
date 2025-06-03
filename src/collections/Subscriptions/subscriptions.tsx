c09a8e5581a42edf9a07eacfcfd65e03a4ee978d
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2025-06-03 20:47:00 +0300

    write subscriptions collection

---
+++ src/collections/Subscriptions/subscriptions.tsx
@@ -0,0 +1,82 @@
+import { buildCollection, buildEnumValues } from '@firecms/core';
+
+const status = buildEnumValues({
+	'active': 'active',
+	'inactive': 'inactive',
+	'failed': 'failed',
+});
+
+export const subscriptionsCollection = buildCollection({
+	name: 'Subscriptions',
+	singularName: 'Subscriptions',
+	id: 'subscriptions',
+	path: 'subscriptions',
+	group: 'Shop',
+	//description: 'data: uk, en, he',
+	textSearchEnabled: true,
+	// Here you can override the user permissions
+	// permissions: ({ authController }) => ({
+	//     read: true,
+	//     edit: true,
+	//     create: true,
+	//     delete: true
+	// }),
+	subcollections: [],
+	entityViews: [
+		//{
+		//  key: 'preview',
+		//  name: 'Sample preview',
+		//  Builder: ProductDetailPreview,
+		//},
+	],
+	properties: {
+		status: {
+			name: 'status',
+			validation: { required: true },
+			dataType: 'string',
+			enumValues: status
+		},
+		amount: {
+			name: 'amount',
+			validation: { required: true },
+			dataType: 'number',
+		},
+		invoiceId: {
+			name: 'invoiceId',
+			validation: { required: true },
+			dataType: 'string',
+		},
+		monobankToken: {
+			name: 'monobankToken',
+			validation: { required: true },
+			dataType: 'string',
+		},
+		userId: {
+			name: 'userId',
+			validation: { required: true },
+			dataType: 'string',
+		},
+		paymentDate: {
+			name: 'paymentDate',
+			validation: { required: true },
+			dataType: 'date',
+		},
+		expiresDate: {
+			name: 'expiresDate',
+			validation: { required: true },
+			dataType: 'date',
+		},
+		createdAt: {
+			name: 'createdAt',
+			validation: { required: true },
+			dataType: 'date',
+			autoValue: "on_create"
+		},
+		updatedAt: {
+			name: 'updatedAt',
+			validation: { required: true },
+			dataType: 'date',
+			autoValue: "on_update"
+		},
+	},
+});
