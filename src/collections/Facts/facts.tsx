4718ffb0fd56974ec7f492b071a36349b2d5b74c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2025-06-07 21:50:48 +0300

    fix facts

--- src/collections/Facts/facts.tsx
+++ src/collections/Facts/facts.tsx
@@ -6,12 +6,13 @@ import { heSubCollection } from './he';
 
 export const factsCollection = buildCollection({
   name: 'Facts modal',
-  singularName: 'Facts',
+  singularName: 'Fact',
   id: 'facts',
   path: 'facts',
 	group: 'Secondary content',
 	icon:'fact_check',
   description: 'data: uk, en, he',
+	propertiesOrder: ['subcollection:uk', 'subcollection:en', 'subcollection:he'],
 	permissions: ({ authController }) => ({
 			read: true,
 			edit: false,
@@ -19,5 +20,16 @@ export const factsCollection = buildCollection({
 			delete: false
 	}),
   subcollections: [ukSubCollection, enSubCollection, heSubCollection],
-  properties: {},
+  properties: {
+		createdAt: {
+			name: 'createdAt',
+			dataType: 'date',
+			autoValue: "on_create"
+		},
+		updatedAt: {
+			name: 'updatedAt',
+			dataType: 'date',
+			autoValue: "on_update"
+		},
+	},
 });
