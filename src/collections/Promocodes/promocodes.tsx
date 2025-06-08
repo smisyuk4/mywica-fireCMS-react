f45ae6fce2e8797623353b5ea1e06627e203fe4b
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2025-06-08 22:04:46 +0300

    write validation in promocodes collection

--- src/collections/Promocodes/promocodes.tsx
+++ src/collections/Promocodes/promocodes.tsx
@@ -1,4 +1,5 @@
 import { buildCollection } from '@firecms/core';
+import { reuseIdCallbacks } from '../../customCallbacks';
 
 export const promocodesCollection = buildCollection({
 	name: 'Promocodes',
@@ -7,59 +8,70 @@ export const promocodesCollection = buildCollection({
 	path: 'promocodes',
 	description: 'discounts for users by subscriptions and souvenirs',
 	group: 'Users',
+	callbacks: reuseIdCallbacks,
 	textSearchEnabled: true,
-	// Here you can override the user permissions
-	// permissions: ({ authController }) => ({
-	//     read: true,
-	//     edit: true,
-	//     create: true,
-	//     delete: true
-	// }),
+	initialSort: ['createdAt', "desc"],
+	permissions: ({ authController }) => ({
+			read: true,
+			edit: true,
+			create: true,
+			delete: true
+	}),
 	properties: {
-		isActual: {
-			name: 'isActual',
-			validation: { required: true },
-			dataType: 'boolean',
-		},
-				description: {
-			name: 'description',
-			validation: { required: true },
-			dataType: 'string',
-		},
 		discount: {
 			name: 'discount',
-			validation: { required: true },
+			validation: { required: true, min: 5, max: 100 },
 			dataType: 'number',
 		},
 		counter: {
 			name: 'counter',
-			validation: { required: true },
+			validation: { required: true, min: 0 },
 			dataType: 'number',
 		},
 		promocode: {
 			name: 'promocode',
+			validation: {
+				required: true,
+				trim: true,
+				matches: /^[A-Za-z0-9_-]+$/,
+				matchesMessage: "Only Latin letters, numbers, _ or -"
+			},
+			dataType: 'string',
+		},
+		isActual: {
+			name: 'isActual',
 			validation: { required: true },
+			dataType: 'boolean',
+			defaultValue: true
+		},
+		description: {
+			name: 'description',
+			validation: { required: true, trim: true },	
 			dataType: 'string',
 		},
 		validityPeriod: {
-			name: 'validityPeriod',
+			name: 'validity Period',
 			validation: { required: true },
 			dataType: 'date',
+			defaultValue: (() => {
+				const date = new Date();
+				date.setMonth(date.getMonth() + 1);
+				return date;
+		})()
 		},
 		id: {
 			name: 'id',
-			validation: { required: true },
 			dataType: 'string',
+			readOnly: true,
+			hideFromCollection: true
 		},
 		createdAt: {
 			name: 'createdAt',
-			validation: { required: true },
 			dataType: 'date',
 			autoValue: "on_create"
 		},
 		updatedAt: {
 			name: 'updatedAt',
-			validation: { required: true },
 			dataType: 'date',
 			autoValue: "on_update"
 		},
